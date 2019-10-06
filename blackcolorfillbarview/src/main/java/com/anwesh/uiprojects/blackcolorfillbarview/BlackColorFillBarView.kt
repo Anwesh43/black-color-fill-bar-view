439dacd401bde5fa171047247ed69c46b244f42c
Anwesh43 <anweshthecool0@gmail.com>
2019-10-06 12:12:44 +0530

    adding Renderer class

--- blackcolorfillbarview/src/main/java/com/anwesh/uiprojects/blackcolorfillbarview/BlackColorFillBarView.kt
+++ blackcolorfillbarview/src/main/java/com/anwesh/uiprojects/blackcolorfillbarview/BlackColorFillBarView.kt
@@ -50,15 +50,16 @@ fun Canvas.drawBCFBNode(i : Int, sc : Float, paint : Paint) {
 class BlackColorFillBarView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val renderer : Renderer = Renderer(this)
 
     override fun onDraw(canvas : Canvas) {
-
+        renderer.render(canvas, paint)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
