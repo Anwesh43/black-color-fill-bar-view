b1e38cd50d4a121b529b980a65364b921e8bdd83
Anwesh43 <anweshthecool0@gmail.com>
2019-10-06 11:29:15 +0530

    adding constants and libraries

--- blackcolorfillbarview/src/main/java/com/anwesh/uiprojects/blackcolorfillbarview/BlackColorFillBarView.kt
+++ blackcolorfillbarview/src/main/java/com/anwesh/uiprojects/blackcolorfillbarview/BlackColorFillBarView.kt
@@ -3,3 +3,18 @@ package com.anwesh.uiprojects.blackcolorfillbarview
 /**
  * Created by anweshmishra on 06/10/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+
+val colors : Array<String> = arrayOf("#4CAF50", "#f44336", "#3F51B5", "#E65100", "#1A237E")
+val scGap : Float = 0.01f
+val sizeFactor : Float = 2.9f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
