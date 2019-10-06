48a986693ee8b4e5d4cb18504493d3a8849d4751
Anwesh43 <anweshthecool0@gmail.com>
2019-10-06 12:15:54 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedblackcolorfillbarview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedblackcolorfillbarview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedblackcolorfillbarview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.blackcolorfillbarview.BlackColorFillBarView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         BlackColorFillBarView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
