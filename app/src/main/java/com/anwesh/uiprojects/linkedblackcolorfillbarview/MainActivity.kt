04881d3f633e1e4b7f0de2610d2c59572fba6310
Anwesh43 <anweshthecool0@gmail.com>
2019-10-06 12:15:11 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedblackcolorfillbarview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedblackcolorfillbarview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedblackcolorfillbarview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.blackcolorfillbarview.BlackColorFillBarView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BlackColorFillBarView.create(this)
     }
 }
