da467074e695fdafe392ba4388528ca6e921adcb
Anwesh43 <anweshthecool0@gmail.com>
2019-10-06 11:53:35 +0530

    adding BCFBNode class

--- blackcolorfillbarview/src/main/java/com/anwesh/uiprojects/blackcolorfillbarview/BlackColorFillBarView.kt
+++ blackcolorfillbarview/src/main/java/com/anwesh/uiprojects/blackcolorfillbarview/BlackColorFillBarView.kt
@@ -111,4 +111,46 @@ class BlackColorFillBarView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BCFBNode(var i : Int, val state : State = State()) {
+
+        private var next : BCFBNode? = null
+        private var prev : BCFBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BCFBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBCFBNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BCFBNode {
+            var curr : BCFBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
